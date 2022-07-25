0e51ea28bd2e01f0f4d96c711a37c2f0d4833dd5
KashinGen <gennady.kashin94@gmail.com>
2022-07-25 19:57:19 +0300

    search component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import Search from './Search';
 
 const Header = () => {
+	const location = useLocation();
+
 	return (
 		<div className="header">
 			<div className="container">
@@ -13,6 +16,8 @@ const Header = () => {
 						</div>
 					</div>
 				</Link>
+				{location.pathname !== '/cart' && <Search />}
+
 				<div className="header__cart">
 					<Link to="/cart" className="button button--cart">
 						<span>520 ₽</span>
